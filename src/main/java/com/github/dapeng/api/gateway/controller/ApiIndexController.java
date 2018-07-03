3a5a98c431609caff1e39cb71f81ea8a51cb1db8
leihuazhe <295482300@qq.com>
2018-07-03 14:29:04 +0800

    change

--- src/main/java/com/github/dapeng/api/gateway/controller/ApiIndexController.java
+++ src/main/java/com/github/dapeng/api/gateway/controller/ApiIndexController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * @author with struy.
  * Create by 2018/4/17 14:21
@@ -20,6 +23,14 @@ public class ApiIndexController {
     public ResponseEntity<?> index() {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body("gateway is working !");
+                .body("gateway is working 2!");
+    }
+
+    @RequestMapping("/check")
+    public ResponseEntity<?> check() throws UnknownHostException {
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("IP:[" + hostAddress + "]: gateway is working 2!");
     }
 }
